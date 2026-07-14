82ed27406b36c648f4248e4d87d989c72aabaa7f
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-07-14 13:02:56 -0300

    first commit

---
+++ backend/src/main/java/com/jcarlos67/biblioneteca/BiblionetecaApplication.java
@@ -0,0 +1,13 @@
+package com.jcarlos67.biblioneteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblionetecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblionetecaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jcarlos67/biblioneteca/BiblionetecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcarlos67.biblioneteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblionetecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
