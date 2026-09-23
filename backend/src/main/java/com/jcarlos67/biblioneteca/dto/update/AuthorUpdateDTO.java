31be65ff47aaf876836a3f4dee4dc10edfb796a3
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-09-23 17:29:42 -0300

    feat: method for updating author data with data validation

---
+++ backend/src/main/java/com/jcarlos67/biblioneteca/dto/update/AuthorUpdateDTO.java
@@ -0,0 +1,23 @@
+package com.jcarlos67.biblioneteca.dto.update;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public record AthorUpdateDTO(
+        @NotBlank(message = "The author's name is required")
+        String name,
+
+        @NotNull(message = "The author's date of birth is mandatory")
+        @PastOrPresent(message = "The date of birth cannot be in the future")
+        LocalDate dateOfBirth,
+
+        @PastOrPresent(message = "The date of birth cannot be in the future")
+        LocalDate dateOfDeath,
+
+        @NotBlank (message = "The author's nationality is required")
+        String nationality
+) {
+}
