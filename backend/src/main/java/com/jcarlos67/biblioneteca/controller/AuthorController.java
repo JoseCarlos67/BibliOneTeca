dfe0b935a84ca2f67a6c0afd10b2e08d2d02272c
JoseCarlos67 <josecarlos.silesteves@gmail.com>
2026-09-23 16:05:24 -0300

    feat: handle blank or null field exceptions during author registration

--- backend/src/main/java/com/jcarlos67/biblioneteca/controller/AuthorController.java
+++ backend/src/main/java/com/jcarlos67/biblioneteca/controller/AuthorController.java
@@ -6,6 +6,7 @@ import com.jcarlos67.biblioneteca.model.collection.Author;
 import com.jcarlos67.biblioneteca.service.AuthorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -45,7 +46,7 @@ public class AuthorController {
   }
 
   @PostMapping
-  public ResponseEntity<AuthorResponseDTO> createAuthor(@RequestBody AuthorCreateDTO newAuthor) {
+  public ResponseEntity<AuthorResponseDTO> createAuthor(@Valid @RequestBody AuthorCreateDTO newAuthor) {
     AuthorResponseDTO authorResponseDTO = service.createAuthor(newAuthor);
 
     return ResponseEntity.status(HttpStatus.CREATED).body(authorResponseDTO);
@@ -56,6 +57,5 @@ public class AuthorController {
     service.delete(id);
     return ResponseEntity.noContent().build();
   }
-
 
 }
--- backend/src/main/java/com/jcarlos67/biblioneteca/dto/create/AuthorCreateDTO.java
+++ backend/src/main/java/com/jcarlos67/biblioneteca/dto/create/AuthorCreateDTO.java
@@ -1,6 +1,8 @@
 package com.jcarlos67.biblioneteca.dto.create;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 
 import java.time.LocalDate;
 
@@ -8,9 +10,14 @@ public record AuthorCreateDTO(
         @NotBlank(message = "The author's name is required")
         String name,
 
-        @NotBlank(message = "The author's date of birth is mandatory")
+        @NotNull(message = "The author's date of birth is mandatory")
+        @PastOrPresent(message = "The date of birth cannot be in the future")
         LocalDate dateOfBirth,
+
+        @PastOrPresent(message = "The date of birth cannot be in the future")
         LocalDate dateOfDeath,
+
+        @NotBlank (message = "The author's nationality is required")
         String nationality
 ) {
 }
